72a2767cc4f6b97e796535457dd232452382963f
bozya02 <bozinandrey1@yandex.ru>
2022-11-12 18:30:25 +0300

    max student count validation

--- ClubSchool/Pages/ClubPage.xaml.cs
+++ ClubSchool/Pages/ClubPage.xaml.cs
@@ -52,8 +52,8 @@ namespace ClubSchool.Pages
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
+            Regex regex = new Regex(@"\b([1-9]|[12][0-5])\b");
+            e.Handled = !regex.IsMatch((sender as TextBox).Text + e.Text);
         }
     }
 }
