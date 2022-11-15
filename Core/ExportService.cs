17f927f3a2ae14fe4b0429c4ac8829d294945d46
bozya02 <bozinandrey1@yandex.ru>
2022-11-15 17:42:45 +0300

    Edit export service

--- Core/ExportService.cs
+++ Core/ExportService.cs
@@ -9,7 +9,7 @@ namespace Core
 {
     public static class ExportService
     {
-        public static void ExportStudentStatistics(List<StudentStatistic> statistics)
+        public static void ExportStatistics(List<StudentStatistic> studentStatistics, List<ClubStatistic> clubStatistics)
         {
             var application = new Excel.Application();
             Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
@@ -23,25 +23,20 @@ namespace Core
             worksheet.Cells[2][1] = "Имя";
             worksheet.Cells[3][1] = "Посещаемость";
 
-            for (int i = 0; i < statistics.Count; i++)
+            for (int i = 0; i < studentStatistics.Count; i++)
             {
-                worksheet.Cells[1][rowIndex] = statistics[i].Student.LastName;
-                worksheet.Cells[2][rowIndex] = statistics[i].Student.FirstName;
-                worksheet.Cells[3][rowIndex] = statistics[i].AttendanceValue;
+                worksheet.Cells[1][rowIndex] = studentStatistics[i].Student.LastName;
+                worksheet.Cells[2][rowIndex] = studentStatistics[i].Student.FirstName;
+                worksheet.Cells[3][rowIndex] = studentStatistics[i].AttendanceValue;
                 rowIndex++;
             }
 
             worksheet.Columns.AutoFit();
             worksheet.Rows.AutoFit();
-            application.Visible = true;
-        }
 
-        public static void ExportClubStatistics(List<ClubStatistic> statistics)
-        {
-            var application = new Excel.Application();
-            Excel.Workbook workbook = application.Workbooks.Add(Type.Missing);
-            Excel.Worksheet worksheet = application.Worksheets.Item[1];
-            int rowIndex = 2;
+            worksheet = application.Worksheets.Add();
+
+            rowIndex = 2;
 
             worksheet.Name = $"Статистика кружков";
             worksheet.Columns.AutoFit();
@@ -49,15 +44,16 @@ namespace Core
             worksheet.Cells[1][1] = "Название крружка";
             worksheet.Cells[2][1] = "Посещаемость";
 
-            for (int i = 0; i < statistics.Count; i++)
+            for (int i = 0; i < clubStatistics.Count; i++)
             {
-                worksheet.Cells[1][rowIndex] = statistics[i].Club.Name;
-                worksheet.Cells[2][rowIndex] = statistics[i].AttendanceValue;
+                worksheet.Cells[1][rowIndex] = clubStatistics[i].Club.Name;
+                worksheet.Cells[2][rowIndex] = clubStatistics[i].AttendanceValue;
                 rowIndex++;
             }
 
             worksheet.Columns.AutoFit();
             worksheet.Rows.AutoFit();
+
             application.Visible = true;
         }
     }
